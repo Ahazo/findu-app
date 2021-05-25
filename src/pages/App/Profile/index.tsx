606f05a3200e897de2c87fe669a31abba680c134
scaralu <scarano.dev@gmail.com>
2021-05-24 23:11:26 -0300

    add user info container and content with progress bar

--- src/pages/App/Profile/index.tsx
+++ src/pages/App/Profile/index.tsx
@@ -1,15 +1,86 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { View, Text, SafeAreaView, Image } from 'react-native';
 
 import Header from '../../../components/Header';
 import { height, width } from '../../../constants';
+import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
+import { UserInfoContainer, UserInfoContent, ImageContainer } from './styles';
+
+import { ProgressBar, Colors } from 'react-native-paper';
+
 
 export function Profile() {
   return (
     <>
       <SafeAreaView style={{flex: 1, borderWidth: 1,}}>
-        <Header heightPercentage={50} hasBackButton={true} logoDimensions={{height: height * 0.06, width: width * 0.2}}/>
+        <Header heightPercentage={30} logoDimensions={{height: height * 0.1, width: width * 0.2}}/>
+ 
+        <UserInfoContainer>
+          <UserInfoContent style={{
+            elevation: 10,
+            shadowColor: colors.body,
+            shadowOffset: {
+              width: 0,
+              height: 5,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.5,
+          }}>
+            <ImageContainer>
+              <Image 
+                source={{
+                  uri: "https://storage.googleapis.com/images-ahazo-dev/dev-images/minhaGata.jpg"
+                }}
+                resizeMode='contain'
+                style={{
+                  height: '61%',
+                  width: '64%',
+                  borderRadius: 20,
+                }}
+              />
+            </ImageContainer>
+            <View style={{
+              flexDirection: 'column',
+              flex: 2
+            }}>
+              <Text style={{
+                fontFamily: fonts.heading,
+                fontSize: 18,
+                color: colors.heading,
+              }}>
+                Júlia Alves
+              </Text>
+              <Text style={{
+                fontFamily: fonts.heading,
+                fontSize: 14,
+                color: colors.body,
+              }}>
+                Mestra da Indicação
+              </Text>
+              <Text style={{
+                fontFamily: fonts.semibold,
+                fontSize: 12,
+                color: colors.body_light,
+              }}>
+                Nivel 3
+              </Text>
+              <ProgressBar progress={0.31} color={colors.purple_dark}/>
+            </View>
+            <ImageContainer>
+              <Image 
+                source={{
+                  uri: "https://storage.googleapis.com/images-ahazo-dev/dev-images/champ.png"
+                }}
+                resizeMode='contain'
+                style={{
+                  height: '100%',
+                  width: '100%',
+                }}
+              />
+            </ImageContainer>
+          </UserInfoContent>
+        </UserInfoContainer>
       </SafeAreaView>
     </>
   );
---
+++ src/pages/App/Profile/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const UserInfoContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 12%;
+  position: absolute;
+  top: 22%;
+`;
+
+export const UserInfoContent = styled.View`
+  background-color: white;
+  width: 90%;
+  height: 100%;
+  border-radius: 25;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const ImageContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
